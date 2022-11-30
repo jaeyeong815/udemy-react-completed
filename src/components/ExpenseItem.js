af9f68791bb399c844bc43bc4d43d44303327e69
jaeyeong815 <jae0ee@naver.com>
2022-11-30 11:21:43 +0900

    1130: ExpenseDate 컴포넌트 분리

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({ title, amount, date }) {
-  const month = date.toLocaleString('ko', { month: 'long' });
-  const day = date.toLocaleString('ko', { day: '2-digit' });
-  const year = date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
