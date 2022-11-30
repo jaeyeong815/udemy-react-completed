2f631412c7dceae5cc99453500ab77e71910567f
jaeyeong815 <jae0ee@naver.com>
2022-11-30 10:55:00 +0900

    1130: props로 자식 컴포넌트에 데이터 전달

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 12, 29).toDateString();
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem({ title, amount, date }) {
+  const month = date.toLocaleString('ko', { month: 'long' });
+  const day = date.toLocaleString('ko', { day: '2-digit' });
+  const year = date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
