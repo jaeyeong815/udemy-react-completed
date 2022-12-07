f8dccd28ad9917c5a0df7f3ebec562edeea714e3
jaeyeong815 <jae0ee@naver.com>
2022-12-07 16:06:04 +0900

    1206: ExpenseItem title 업데이트 기능 일부 구현

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }) => {
+  let [expenseTitle, setExpenseTitle] = useState(title);
+  const clickHandler = () => {
+    setExpenseTitle('updated');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
