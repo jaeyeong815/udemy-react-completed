916e80148ad33320238c253aac203d706d923b2f
jaeyeong815 <jae0ee@naver.com>
2022-12-08 13:16:42 +0900

    1208: expenses map으로 리스트 렌더링하기

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -17,21 +17,13 @@ const Expense = ({ expenses }) => {
         selected={filteredYear}
         onFilterExpense={filterChangeHandler}
       />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
