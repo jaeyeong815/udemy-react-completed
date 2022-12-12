733f2dd80055553b43da339ca8e880ef81218ad8
jaeyeong815 <jae0ee@naver.com>
2022-12-12 17:27:31 +0900

    feat: AddUser 동작 시 유효성 검사 로직 추가 (임시로 console에 출력)

--- addUserService/src/components/Users/AddUser.js
+++ addUserService/src/components/Users/AddUser.js
@@ -7,27 +7,42 @@ const AddUser = () => {
   const [age, setAge] = useState('');
 
   const usernameInputHandler = (e) => {
-    console.log('username', username);
     setUsername(e.target.value);
   };
 
   const ageInputHandler = (e) => {
-    console.log('age', age);
     setAge(e.target.value);
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('전송');
+
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      console.log('입력값이 비어있습니다');
+      return;
+    }
+
+    if (Number(age) < 1) {
+      console.log('나이를 다시 확인해주세요');
+      return;
+    }
+
+    setUsername('');
+    setAge('');
   };
 
   return (
     <Card>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">User Name</label>
-        <input type="text" id="username" onChange={usernameInputHandler} />
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={usernameInputHandler}
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input type="text" id="age" onChange={ageInputHandler} />
+        <input type="text" id="age" value={age} onChange={ageInputHandler} />
         <Button content="Add User" type="submit" />
       </form>
     </Card>
