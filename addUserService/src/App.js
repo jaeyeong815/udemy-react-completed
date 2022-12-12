5ddc7905014f60b2d0dca0b5ead83878703dbadb
jaeyeong815 <jae0ee@naver.com>
2022-12-12 17:41:44 +0900

    feat: UserList 컴포넌트 레이아웃

--- addUserService/src/App.js
+++ addUserService/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  // 임시 데이터
+  const userList = [
+    { name: 'Max', age: '31' },
+    { name: 'Jae', age: '32' },
+  ];
   return (
     <div>
       <AddUser />
+      <UserList userList={userList} />
     </div>
   );
 }
