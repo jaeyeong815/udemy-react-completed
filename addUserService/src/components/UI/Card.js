2937bf165d2c1d0b165e29a3f9e93b50826fbc3c
jaeyeong815 <jae0ee@naver.com>
2022-12-13 10:51:13 +0900

    style: Card 공용 스타일 아닌 부분 제거

--- addUserService/src/components/UI/Card.js
+++ addUserService/src/components/UI/Card.js
@@ -3,45 +3,12 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background-color: white;
   border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
 
-  margin: 2rem auto;
-  padding: 1rem;
   width: 90%;
   max-width: 40rem;
 
-  & form {
-    display: flex;
-    flex-direction: column;
-
-    & button {
-      width: 200px;
-      border: none;
-      background-color: purple;
-      color: white;
-      border-radius: 5px;
-      padding: 10px 0;
-    }
-  }
-
-  & label {
-    font-weight: bold;
-    margin-bottom: 0.5rem;
-    display: block;
-  }
-
-  & input {
-    display: block;
-    font: inherit;
-    width: 100%;
-    border: 1px solid #ccc;
-    padding: 0.15rem;
-    margin-bottom: 0.5rem;
-
-    :focus {
-      outline: none;
-      border-color: #4f005f;
-    }
-  }
+  margin: 2rem auto;
 `;
 
 const Card = ({ children }) => {
