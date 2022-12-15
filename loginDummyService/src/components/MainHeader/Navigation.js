3da02b9e9cb72f699d4335729e22bea3e15912fd
jaeyeong815 <jae0ee@naver.com>
2022-12-15 12:10:12 +0900

    feat: useContext 사용하여 Navigation isLoggedIn 데이터 관리

--- loginDummyService/src/components/MainHeader/Navigation.js
+++ loginDummyService/src/components/MainHeader/Navigation.js
@@ -1,22 +1,24 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { AuthContext } from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const auth = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {auth.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {auth.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {auth.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
