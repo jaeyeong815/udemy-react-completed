5b066059f2b63bfe838c9b5083c4578a9dc9c333
jaeyeong815 <jae0ee@naver.com>
2022-12-22 13:20:54 +0900

    feat: Counter 컴포넌트에 redux 연결

--- redexCounter/src/components/Counter.js
+++ redexCounter/src/components/Counter.js
@@ -1,12 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>증가</button>
+        <button onClick={decrementHandler}>감소</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
