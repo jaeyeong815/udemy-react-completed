b309974213e00b97822b3d2838a6381718ff551b
jaeyeong815 <jae0ee@naver.com>
2022-12-23 09:17:49 +0900

    feat: Header, Auth 기능 추가

--- redexCounter/src/store/index.js
+++ redexCounter/src/store/index.js
@@ -25,10 +25,28 @@ const counterSlice = createSlice({
   },
 });
 
+const initAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initAuthState,
+  reducers: {
+    login: (state) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
