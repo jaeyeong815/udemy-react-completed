0256576642524b469227e37737c0f5fdc8e9d710
jaeyeong815 <jae0ee@naver.com>
2022-12-26 15:58:44 +0900

    refactor: 페이지 첫 방문 시 데이터 로딩
    useEffect, useCallback Hook 사용

--- starWars/src/App.js
+++ starWars/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchApiHandler = async () => {
+  const fetchApiHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -30,7 +30,11 @@ function App() {
       setError(err.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchApiHandler();
+  }, [fetchApiHandler]);
 
   let content;
 
