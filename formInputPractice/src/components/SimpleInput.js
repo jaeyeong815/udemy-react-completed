b5b2113bf10db9e7142e66c1916887467de1b561
jaeyeong815 <jae0ee@naver.com>
2022-12-29 17:47:10 +0900

    feat: input 값 읽는 방법 1 (useState, onChange 사용)

--- formInputPractice/src/components/SimpleInput.js
+++ formInputPractice/src/components/SimpleInput.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const inputHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log('enteredName' + enteredName);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={enteredName} onChange={inputHandler} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
