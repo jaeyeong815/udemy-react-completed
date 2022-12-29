61dcd1b9894064da9930b0b7739065c3008ea659
jaeyeong815 <jae0ee@naver.com>
2022-12-29 18:04:46 +0900

    feat: 입력값 유효성 검사

--- formInputPractice/src/components/SimpleInput.js
+++ formInputPractice/src/components/SimpleInput.js
@@ -1,19 +1,29 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
+  const [enteredValueValid, setEnteredValueValid] = useState(true);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     const enteredValue = nameInputRef.current.value;
+
+    if (enteredValue.trim() === '') {
+      setEnteredValueValid(false);
+      return;
+    }
+    setEnteredValueValid(true);
     console.log('enteredValue' + enteredValue);
   };
 
+  const nameInputClasses = enteredValueValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={submitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' ref={nameInputRef} />
+        {!enteredValueValid && <p className='error-text'>이름을 입력해주세요.</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
