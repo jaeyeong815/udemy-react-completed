cf68aa956ab07ed65da5ca5adbb560a5ea8a19d6
jaeyeong815 <jae0ee@naver.com>
2022-12-29 17:47:30 +0900

    feat: input 값 읽는 방법 2 (useRef 사용)

--- formInputPractice/src/components/SimpleInput.js
+++ formInputPractice/src/components/SimpleInput.js
@@ -1,22 +1,19 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-
-  const inputHandler = (e) => {
-    setEnteredName(e.target.value);
-  };
+  const nameInputRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    console.log('enteredName' + enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log('enteredValue' + enteredValue);
   };
 
   return (
     <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' value={enteredName} onChange={inputHandler} />
+        <input type='text' id='name' ref={nameInputRef} />
       </div>
       <div className='form-actions'>
         <button>Submit</button>
