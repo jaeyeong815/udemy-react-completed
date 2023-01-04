b9e330354784c2eda65bff893e29cb5cfa89608d
jaeyeong815 <jae0ee@naver.com>
2023-01-04 22:40:21 +0900

    refactor: http 통신 코드 slice 파일로 이동하여 기능 분리

--- reduxCart/src/App.js
+++ reduxCart/src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { showNotification } from './store/uiSlice';
+import { sendCartData } from './store/cartSlice';
 
 let isIntitial = true;
 
@@ -15,58 +15,18 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        showNotification({
-          status: 'pending',
-          title: '보내는 중...',
-          message: '장바구니 데이터를 보내고 있습니다!',
-        })
-      );
-      const response = await fetch(
-        'https://react-http-cc64a-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'POST',
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        showNotification({
-          status: 'success',
-          title: '성공!',
-          message: '성공적으로 장바구니에 담았습니다.',
-        })
-      );
-    };
-
     if (isIntitial) {
       isIntitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        showNotification({
-          status: 'error',
-          title: '오류 발생!',
-          message: '장바구니에 담는 도중 요류가 발생했어요.',
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
     <>
       {notification && (
-        <Notification
-          status={notification.status}
-          title={notification.title}
-          message={notification.message}
-        />
+        <Notification status={notification.status} title={notification.title} message={notification.message} />
       )}
       <Layout>
         {showCart && <Cart />}
